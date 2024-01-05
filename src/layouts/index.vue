b909a281aad3e13fceb1a8f0e94bec08bcf7da6f
changpengyuan <wbchangpengyuan@lattebank.com>
2024-01-05 19:39:09 +0800

    feat: add layout

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { Layout, LayoutSider, LayoutHeader, LayoutContent } from 'ant-design-vue'
+import { ref } from 'vue'
+</script>
+
 <template>
-  
+  <Layout>
+    <LayoutSider></LayoutSider>
+  </Layout>
 </template>
